97c22e4c01e5b89d46880ad9c23430a1fa1f5c10
Fae J <netta.jordaan123@gmail.com>
2025-04-09 23:32:01 +0200

    wesbite?

--- Woondivo/src/App.vue
+++ Woondivo/src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
-//add in component imports
+import LandingPage from "./components/LandingPage.vue";
 </script>
 
 <template>
+  <div class="AppContainer">
+    <div class="pageContainer">
+      <div class="pageHeader">
+        <div class="pageContent">
+          <LandingPage />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ Woondivo/src/components/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="LandingPage">
+        <div class="Overlay">
+            <h1 class="title">De Woondiversiteit</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LandingPage"
+};
+</script>
